08ec90fffd4fd4254564079a2894672b4aa21825
justdheja <justdheja@gmail.com>
2019-05-26 10:52:26 +0700

    push

--- js/script.js
+++ js/script.js
@@ -3,17 +3,7 @@ function tampilkanSemuaMenu(){
     $.getJSON('json/pizza.Json', function(data){
         let menu = data.menu;
         $.each(menu, function (i, data) { 
-            $('#daftar-menu').append(`
-            <div class="col-md-4">
-            <div class="card mb-3" style="width: 18rem;">
-                <img class="card-img-top" src="img/pizza/'+ data.gambar +'" alt="Card image cap">
-                <div class="card-body"><h5 class="card-title">'+ data.nama +
-                '</h5><p class="card-text">'+ data.deskripsi +
-                '</p><h5 class="card-title">'+ data.harga +
-                '</h5><a href="#" class="btn btn-primary">order now</a>
-                </div>
-            </div>
-            </div>`)
+            $('#daftar-menu').append(`<div class="col-md-4"><div class="card mb-3" style="width: 18rem;"><img class="card-img-top" src="img/pizza/`+ data.gambar +`" alt="Card image cap"><div class="card-body"><h5 class="card-title">`+ data.nama +`</h5><p class="card-text">`+ data.deskripsi +`</p><h5 class="card-title">`+ data.harga +`</h5><a href="#" class="btn btn-primary">order now</a></div></div></div>`)
         });
     
     });
